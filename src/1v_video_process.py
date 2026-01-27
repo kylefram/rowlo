62e68b8ccb4bc06c7671d04f5a884f0d792f6de4
kylefram <kdf25@cam.ac.uk>
2026-01-27 18:32:36 +0000

    revert to v1

--- src/1v_video_process.py
+++ src/1v_video_process.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import pandas as pd
 from ultralytics import YOLO
+from tqdm import tqdm
+import scipy.stats
 
 DEVICE="mps"
 #VIDEO_PATH = "../input_videos/lifting.mov"
@@ -11,11 +13,11 @@ VIDEO_PATH = "input_videos/avis5.mov"
 DETECTION_MODEL_PATH = "models/yolo26x.pt"
 POSE_MODEL_PATH = "models/yolo26x-pose.pt"
 #TRACK_MODEL="bytetrack.yaml"
-TRACK_MODEL="botsort.yaml"
+TRACK_MODEL="models/botsort.yaml"
 
 
 OUTPUT_VIDEO_PATH = "output_videos/"
-OUTPUT_CSV_PATH = "output_data/output.csv"
+OUTPUT_CSV_DIR = "output_data/"
 
 PADDING=.3
 #https://docs.ultralytics.com/tasks/pose/
@@ -54,7 +56,7 @@ def parse_args():
     p.add_argument("--pose_model", type=str, default=POSE_MODEL_PATH, required=False, help="Path to yolov8 pose weights (-pose.pt)")
     p.add_argument("--tracker", type=str, default=TRACK_MODEL, required=False, help="Path to tracker config file")
     p.add_argument("--output_video", type=str, default=OUTPUT_VIDEO_PATH, required=False, help="Path to save the output video file")
-    p.add_argument("--output_csv", type=str, default=OUTPUT_CSV_PATH, required=False, help="Path to save the output CSV file")
+    p.add_argument("--output_csv_dir", type=str, default=OUTPUT_CSV_DIR, required=False, help="Path to save the output CSV file")
     p.add_argument("--max_frames", type=int, default=60, required=False, help="Maximum number of frames to process from the video")
 
     return p.parse_args()
@@ -67,11 +69,7 @@ def compute_box(x1,y1,x2,y2,padding,width,height):
     return x1, y1, x2, y2
 
 def main():
-    data = []
     args = parse_args()
-
-    detection = YOLO(args.detection_model)
-    pose  = YOLO(args.pose_model)
 
     cap = cv2.VideoCapture(args.video)
     width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -85,54 +83,137 @@ def main():
     output_video = args.output_video + f"{input_name}_annotated.mp4"
     out = cv2.VideoWriter(output_video, fourcc, fps, (width, height))
 
-    print("Starting video processing...")
+    #first pass: track boat and rowers
+    data = process_video(args,cap,width,height)
+    df = pd.DataFrame(data)
+    df = df_postprocess(df)
 
-    i=0
-    while cap.isOpened():
+    df,out = second_pass(args,cap,out,width,height,df)
+
+    output_csv = args.output_csv_dir + f"{input_name.split('.')[0].split('/')[-1]}_data.csv"
+    df.to_csv(output_csv)
+
+    #second pass, crop video and then draw skeletons and charts below 
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    #save video 
+    print(f"Annotated video saved to {output_video}")
+    return df
+
+#dynamic crop, drawing skeletons, and adding charts
+def second_pass(args,cap,out,w,h,df,alpha=0.7):
+
+    g_x1 = [df['x1'].min()]
+    g_y1 = [df['y1'].min()]
+    g_x2 = [df['x2'].max()]
+    g_y2 = [df['y2'].max()]
+
+    for i in tqdm(range(args.max_frames),desc="Annotating frames:"):
         ret, frame = cap.read()
         if not ret:
+            continue
+
+        if not cap.isOpened():
             break
         
+        if not i in df['frame'].values:
+            #if we don't have data for this frame, skip
+            continue
+
+        #define the box
+        min_x = df.loc[df['frame'] == i, 'x1'].min()
+        min_y = df.loc[df['frame'] == i, 'y1'].min()
+        max_x = df.loc[df['frame'] == i, 'x2'].max()
+        max_y = df.loc[df['frame'] == i, 'y2'].max()
+        #exponential smooth
+        g_x1.append(alpha * min_x + (1 - alpha) * g_x1[-1])
+        g_y1.append(alpha * min_y + (1 - alpha) * g_y1[-2])
+        g_y2.append(alpha * max_x + (1 - alpha) * g_y2[-1])
+        g_x2.append(alpha * max_y + (1 - alpha) * g_x2[-1])
+
+    return df,out
+
+#go through the video once, track rowers and boats, save data to dataframe
+def process_video(args,cap,out,w,h):
+    detection = YOLO(args.detection_model)
+    pose  = YOLO(args.pose_model)
+    data=[]
+    for i in tqdm(range(args.max_frames),desc="Processing frames:"):
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        if not cap.isOpened():
+            break
+
         #white canvas
         #canvas = np.ones((height, width, 3), dtype=np.uint8) * 255
 
-        detection_results = detection.track(frame, persist=True, tracker=args.tracker, classes=[0],device=args.device)[0]
+        detection_results = detection.track(frame, persist=True, tracker=args.tracker, classes=[0],device=args.device,verbose=False)[0]
         boxes = detection_results.boxes
         ids = detection_results.boxes.id.cpu().numpy()
 
         for id, box in zip(ids, boxes):
             if box.cls[0] == 0:  #person         
                 x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = compute_box(x1, y1, x2, y2, PADDING, width, height)
+                x1, y1, x2, y2 = compute_box(x1, y1, x2, y2, PADDING, w, h)
 
                 person_crop = frame[y1:y2, x1:x2]
-                pose_results = pose(person_crop, device=args.device)[0]
+                pose_results = pose(person_crop, device=args.device,verbose=False)[0]
         
-                draw_skeleton(frame[y1:y2, x1:x2], pose_results.keypoints.data.cpu().numpy())
+                #draw_skeleton(frame[y1:y2, x1:x2], pose_results.keypoints.data.cpu().numpy())
                 #draw_skeleton(canvas[y1:y2, x1:x2], pose_results.keypoints.data.cpu().numpy())
                 data.extend(database_append_rower(i, id, x1, x2, y1, y2, pose_results.keypoints.data.cpu().numpy()))
             elif box.cls[0] == 8:  # boat
                 print("Boat detected")
                 # Handle boat detection if needed
                 data.append(database_append_boat(i, id, x1, y1, x2, y2))
-                
-        #frame = np.vstack((frame, canvas))
-        out.write(frame)
-        cv2.imshow("Annotated Frame", frame)
-        k = cv2.waitKey(3) & 0xFF
-        i+=1
-        if i>args.max_frames:
-            break
+    return data
+        
 
-    df = pd.DataFrame(data)
-    df = df.set_index(["frame", "id", "keypoint"]).sort_index()
-    df.to_csv(args.output_csv)
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-    return df
+def df_postprocess(df,seat_encoding=DEF_BOAT):
+    #group by frame and keypoint to compute distances
+    dfg=df.groupby(['frame','keypoint']).apply(distance_func,include_groups=False).reset_index()
+    #assign seats
+    seat_positions = {}
+    for _, row in dfg.iterrows():
+        ordered_ids = row['ordered_ids']
+        for pos, id_val in enumerate(ordered_ids):
+            seat_positions.setdefault(id_val, []).append(pos)
     
+    # Compute seat_map using mode of positions
+    seat_map = {}
+    for id_val, locs in seat_positions.items():
+        if locs:
+            mode_pos = scipy.stats.mode(locs, keepdims=False).mode
+            seat_map[id_val] = int(mode_pos) + 1  # Seat numbers start at 1
+        else:
+            seat_map[id_val] = -1
+    
+    df['seat_label']=df['id'].map(lambda x: seat_map.get(x,-1))
+    df['seat_side']=df['seat_label'].map(lambda x: seat_encoding.get(x,'unknown'))
+    return df
+
+
+
+def distance_func(x):
+    # Sort keypoints from right to left (assuming higher x is right)
+    x = x.sort_values('x', ascending=False).reset_index(drop=True)
+    ordered_ids = x['id'].tolist()
+    distances = []
+    for i in range(len(x) - 1):
+        # Compute signed L1 distance between consecutive keypoints
+        dx = x.iloc[i]['x'] - x.iloc[i + 1]['x']
+        distances.append(dx)
+    d = {}
+    d['ordered_ids'] = ordered_ids
+    #might want these later! but for now not necessary
+    #d['distances'] = distances
+    #d['x'] = x['x']
+    #d['y'] = x['y']
+
+    return pd.Series(d)
 
 def draw_skeleton(frame,keypoints):
     if keypoints is None or len(keypoints) == 0:
--- src/2v_video_process.py
+++ src/2v_video_process.py
@@ -7,11 +7,10 @@ from ultralytics import YOLO
 
 DEVICE="mps"
 #VIDEO_PATH = "../input_videos/lifting.mov"
-VIDEO_PATH = "input_videos/avis4.mov"
+VIDEO_PATH = "input_videos/avis6.mov"
 DETECTION_MODEL_PATH = "models/yolo26x.pt"
 POSE_MODEL_PATH = "models/yolo26x-pose.pt"
-#TRACK_MODEL="bytetrack.yaml"
-TRACK_MODEL="botsort.yaml"
+TRACK_MODEL="models/botsort.yaml"
 
 DEF_BOAT = {1:'s',2:'p',3:'s',4:'p',5:'s',6:'p',7:'s',8:'p',9:'c'}
 
@@ -53,13 +52,20 @@ def main(args):
     output_video = args.output_video + f"{input_name}_annotated.mp4"
     out = cv2.VideoWriter(output_video, fourcc, fps, (width, height))
 
-    boat_df = boat_track(cap,out,args)
-
-    rower_df = rower_track(cap,out,args)
+    #pass 1: track boat
+    boat_df = boat_track(cap,args,width,height)
+    #pass 2: track rowers within boat box
+    rower_df = rower_track(cap,args,boat_df,width,height)
 
     #first pass: identifty boat box
 
-def rower_track(cap,out,args,boat_df):
+def skeleton_identify(cap,args,):
+    pass
+
+
+def rower_track(cap,args,boat_df,w,h):
+    cap = cv2.VideoCapture(args.video)
+
     #track humans within boat box
     rower_data = []
     i=0
@@ -68,59 +74,85 @@ def rower_track(cap,out,args,boat_df):
         if not ret:
             break
 
-        
+        #certain frames may not have been repaired
+        if i in boat_df.index:
+            x1,y1,x2,y2 = map(int, boat_df.loc[i,['x1','y1','x2','y2']])
+        else:
+            print(f"Frame {i} missing boat data, using full frame for rower detection")
+            x1,y1,x2,y2 = None,None,None,None
 
         #detect rowers
-        detection_results_rower,row = detect_rower(i, frame, args)
+        detection_results_rower,row = detect_rower(i, frame[y1:y2, x1:x2], args, x1,y1)
 
         if len(detection_results_rower.boxes) > 0:
-            #we want to find the center of the rower box and move it
-            pass
+            
+            for box in detection_results_rower.boxes:
+                #we want to find the center of the rower box and move it
+                frame = draw_rower_box(frame, box, w, h,x1,y1)
+                rower_data.extend(row)
+                #cv2.imshow("Rower Frame", frame)
 
         i += 1
         if i >= args.max_frames:
             break
 
     cap.release()
-    out.release()
     cv2.destroyAllWindows()
 
     # Save rower data to DataFrame and CSV
     df = pd.DataFrame(rower_data)
+    df = postprocess_box_data(df,i-1)
     input_video_name = args.video.split("/")[-1].split(".")[0]
     output_path = OUTPUT_DATA_PATH + input_video_name + "_rower_output.csv"
-    df.to_csv(output_path, index=False)
+    df.to_csv(output_path, index=True)
     print(f"Rower detection data saved to {output_path}")
 
     return df
 
-def detect_rower(i,frame,args):
+def draw_rower_box(frame, box, fr_width, fr_height,bx1, by1, ver_pad=0.2, hor_pad=0.2):
+    x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+    x1 += bx1
+    y1 += by1
+    x2 += bx1
+    y2 += by1
+    bx_width = x2 - x1
+    bx_height = y2 - y1
+    x1 = max(0, int(x1 - hor_pad * bx_width))
+    x2 = min(fr_width, int(x2 + hor_pad * bx_width))
+    #adjust y1 (top) upwards (recall images increase downwards)
+    y1 = max(0, int(y1 - ver_pad * bx_height))
+    y2 = min(fr_height, int(y2 + ver_pad * bx_height))
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    return frame
+
+def detect_rower(i, frame, args, bx1,by1):
     rower_data = []
     detection = YOLO(args.detection_model)
     results = detection.track(frame, persist=True,
                                classes=[0], 
                                device=args.device, verbose=False)[0]
     for box in results.boxes:
-        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+        x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
         conf = box.conf[0].cpu().numpy()
         cls = box.cls[0].cpu().numpy()
-        id = box.id[0].cpu().numpy() if box.id is not None else -1
+        id = box.id[0].item() if box.id is not None else -1
         rower_data.append({
             'frame': i,
             'id': id,
-            'x1': x1,
-            'y1': y1,
-            'x2': x2,
-            'y2': y2,
+            'x1': x1 + bx1,
+            'y1': y1 + by1,
+            'x2': x2 + bx1,
+            'y2': y2 + by1,
             'confidence': conf,
             'class': cls
         })
 
     return results, rower_data
 
-def boat_track(cap,out,args):
+def boat_track(cap,args,w,h):
     # Initialize list to collect boat detection data
     boat_data = []
+
     i=0
     while cap.isOpened():
         ret, frame = cap.read()
@@ -132,10 +164,9 @@ def boat_track(cap,out,args):
 
         if len(detection_results_boat.boxes) > 0:
             #we want to find the center of the boat box and move it
-            frame = draw_boat_box(frame, detection_results_boat.boxes[0], width, height)
+            frame = draw_boat_box(frame, detection_results_boat.boxes[0], w, h)
             boat_data.extend(row)
-            out.write(frame)
-            cv2.imshow("Annotated Frame", frame)
+            #cv2.imshow("Boat Frame", frame)
         else:
             #often, no boat is detected. TODO: cache last boat position and interpolate movement 
             continue
@@ -145,25 +176,61 @@ def boat_track(cap,out,args):
             break
 
     cap.release()
-    out.release()
     cv2.destroyAllWindows()
 
     # Save boat data to DataFrame and CSV
     df = pd.DataFrame(boat_data)
-    df = postprocess_boat_data(df)
+    df = postprocess_box_data(df,i-1)
     input_video_name = args.video.split("/")[-1].split(".")[0]
     output_path = OUTPUT_DATA_PATH + input_video_name + "_boat_output.csv"
-    df.to_csv(output_path, index=False)
+    df.to_csv(output_path, index=True)
     print(f"Boat detection data saved to {output_path}")
 
     return df
 
-def postprocess_boat_data(df):
+def postprocess_box_data(df,n):
     #only take the most common id. TODO: improve to persist id across frames
-    df = df[df['id'] == df['id'].map(pd.Series.mode)]
+    df = correct_ids_simple(df)
+    df = df.drop_duplicates(subset='frame')
     df.set_index('frame', inplace=True)
-    #fill in missing frames by interpolation
-    df[['frame','x1', 'y1', 'x2', 'y2']] = df[['frame','x1', 'y1', 'x2', 'y2']].interpolate(method='index', limit_direction='both')
+
+    interpolated_dfs = []
+    full_index = pd.RangeIndex(start=0, stop=n, step=1)
+    for id_val, group in df.groupby('id'):
+        group = group.reindex(full_index)
+        group[['x1', 'y1', 'x2', 'y2']] = group[['x1', 'y1', 'x2', 'y2']].interpolate(method='index', limit_direction='both')
+        group['id'] = id_val  # Restore id column
+        interpolated_dfs.append(group.reset_index())
+    
+    df = pd.concat(interpolated_dfs, ignore_index=True)
+
+    return df
+
+def correct_ids_simple(df, dist_thresh=50, max_gap=5):
+    df = df.sort_values(['frame', 'id']).copy()
+    track_centers = {}  # id -> (last_frame, center_x, center_y)
+    
+    for idx, row in df.iterrows():
+        frame, id_val = row['frame'], row['id']
+        cx = (row['x1'] + row['x2']) / 2
+        cy = (row['y1'] + row['y2']) / 2
+        
+        # Find closest previous track within gap
+        min_dist = float('inf')
+        best_id = None
+        for tid, (last_frame, px, py) in track_centers.items():
+            if frame - last_frame <= max_gap:
+                dist = ((cx - px)**2 + (cy - py)**2)**0.5
+                if dist < min_dist:
+                    min_dist = dist
+                    best_id = tid
+        
+        if best_id and min_dist < dist_thresh:
+            df.at[idx, 'id'] = best_id
+            track_centers[best_id] = (frame, cx, cy)
+        else:
+            track_centers[id_val] = (frame, cx, cy)
+    
     return df
 
 def detect_boat(i,frame,args):
@@ -175,10 +242,10 @@ def detect_boat(i,frame,args):
                                classes=[8], 
                                device=args.device, verbose=False)[0]
     for box in results.boxes:
-        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+        x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
         conf = box.conf[0].cpu().numpy()
         cls = box.cls[0].cpu().numpy()
-        id = box.id[0].cpu().numpy() if box.id is not None else -1
+        id = box.id[0].item() if box.id is not None else -1
         boat_data.append({
             'frame': i,
             'id': id,
